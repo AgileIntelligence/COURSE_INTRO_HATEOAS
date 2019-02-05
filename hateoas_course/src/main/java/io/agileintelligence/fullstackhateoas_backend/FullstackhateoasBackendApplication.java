b5059ff381217a5cd9c06bbaca0aec84d6fa91e4
AgileIntelligence <io.agileintelligence@gmail.com>
2019-02-05 14:29:11 -0500

    first commit

---
+++ hateoas_course/src/main/java/io/agileintelligence/fullstackhateoas_backend/FullstackhateoasBackendApplication.java
@@ -0,0 +1,14 @@
+package io.agileintelligence.fullstackhateoas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackhateoasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackhateoasBackendApplication.class, args);
+    }
+
+}
+
---
+++ hateoas_course/src/test/java/io/agileintelligence/fullstackhateoas_backend/FullstackhateoasBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package io.agileintelligence.fullstackhateoas_backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullstackhateoasBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
