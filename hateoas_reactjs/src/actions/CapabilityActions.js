29f0ea67ec26f4dc3e9a9b93dba51224e4b97b26
AgileIntelligence <io.agileintelligence@gmail.com>
2019-02-09 20:53:49 -0500

    bug fixes

--- hateoas_reactjs/src/actions/CapabilityActions.js
+++ hateoas_reactjs/src/actions/CapabilityActions.js
@@ -2,12 +2,18 @@ import axios from "axios";
 import { GET_CAPABILITIES, DELETE_CAPABILITY } from "./ActionTypes";
 
 export const getAllCapabilities = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/dashboard");
-
-  dispatch({
-    type: GET_CAPABILITIES,
-    payload: res.data._embedded.capabilityList
-  });
+  try {
+    const res = await axios.get("http://localhost:8080/dashboard");
+    dispatch({
+      type: GET_CAPABILITIES,
+      payload: res.data._embedded.capabilityList
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_CAPABILITIES,
+      payload: []
+    });
+  }
 };
 
 export const deleteCapability = (id, deleteLink) => async dispatch => {
--- hateoas_reactjs/src/components/CapabilityTool/Capability.js
+++ hateoas_reactjs/src/components/CapabilityTool/Capability.js
@@ -49,6 +49,11 @@ export class Capability extends Component {
   }
 }
 
+Capability.propTypes = {
+  capability: PropTypes.object.isRequired,
+  deleteCapability: PropTypes.func.isRequired
+};
+
 export default connect(
   null,
   { deleteCapability }
